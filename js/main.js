488d3d979d7c2253e034a3e45c3f8ce6c71f6242
KylieTKay <113371037+KylieTKay@users.noreply.github.com>
2023-02-28 02:10:19 -0500

    mobiles done set up

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// let mobile = window.matchMedia('(max-width: 600px)');
+//     // logo = document.querySelector("#logo");
+
+
+// function changeImage(mobile) {
+//     if (mobile.matches) {
+// 	    document.querySelector("#logo").src = `https://via.placeholder.com/130x95/838383/efefef`;
+//     }
+//     else {
+// 	    document.querySelector("#logo").src = `https://via.placeholder.com/270x95/838383/efefef`;
+//     }
+// }
+
+// mobile.addEventListener('change', changeImage);
+
+
